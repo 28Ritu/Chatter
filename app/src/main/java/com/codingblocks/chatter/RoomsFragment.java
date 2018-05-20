12a8614849aa2e318ce5c46436a60b137455e400
Ritu <ritu16078@iiitd.ac.in>
2018-05-20 11:29:15 +0530

    Finally resolved issue 26

--- app/src/main/java/com/codingblocks/chatter/RoomsFragment.java
+++ app/src/main/java/com/codingblocks/chatter/RoomsFragment.java
@@ -50,7 +50,7 @@ public class RoomsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_rooms, container, false);
-        ButterKnife.bind(view);
+        ButterKnife.bind(this, view);
 
         Realm.init(getActivity().getApplicationContext());
         Realm realm = Realm.getDefaultInstance();
